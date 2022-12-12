c63eed6d2fd0d79eca758c03b0ffbfc1c3f46a4d
ShushoB <114217901+ShushoB@users.noreply.github.com>
2022-12-12 17:03:35 +0400

    Update froggyland.py

--- froggyland.py
+++ froggyland.py
@@ -5,59 +5,69 @@ sunrise = 7
 sunset = 21
 #hour = 3
 
-class Sun:
-    def light(self, hour):
+
+class Utility:
+    def isDay(self, hour):
         if hour >= sunrise and hour <= sunset:
+            return True
+        else:
+            return False
+
+class Sun(Utility):
+    def light(self, hour):
+        if Utility.isDay(self, hour):
             print("The Sun is shining")
         else:
             print("The night has come")
 
-class Tree:
+class Tree(Utility):
     def photosynthesys(self, hour):
-        if hour >= sunrise and hour <= sunset:
+        if Utility.isDay(self, hour):
             print("The tree produces some fresh air")
         else:
             print("The tree is waiting for dark times to end")
 
 class Grass:
     def grow(self, hour):
-        if hour >= sunrise and hour <= sunset:
+        if Utility.isDay(self, hour):
             print("The green grass grows")
         else:
             print("The grass stopped growing")
 
 class Air:
     def exist(self, hour):
-        if hour >= sunrise and hour <= sunset:
+        if Utility.isDay(self, hour):
             print("There's enough air to breath")
         else:
             print("No fresh air is being produced")
 
 class Frog:
+    def __init__(self, name):
+        self.name = name
     def sleep(self, hour):
-        if hour >= sunrise and hour <= sunset:
-            print("The frog is awake")
+        if Utility.isDay(self, hour):
+            print(self.name + " is awake")
         else:
-            print("The frog is sleeping, tsss")
+            print(self.name + " is sleeping, tsss")
 
     def breath(self, hour):
-        if hour >= sunrise and hour <= sunset:
-            print("The frog breathes")
+        if Utility.isDay(self, hour):
+            print(self.name + " breathes")
 
     def eat(self, hour):
-        if hour >= sunrise and hour <= sunset:
-            print("The frog eats fresh grass")
+        if Utility.isDay(self, hour):
+            print(self.name + " eats fresh grass")
 
     def move(self, hour):
-        if hour >= sunrise and hour <= sunset:
-            print("The frog travels across the Froggyland")
+        if Utility.isDay(self, hour):
+            print(self.name + " travels across the Froggyland")
 
 
 theSun = Sun()
 oak = Tree()
 greenGrass = Grass()
 freshAir = Air()
-froggy = Frog()
+froggy = Frog("Froggy")
 
 print("Let's see what happens in Froggyland at this hour")
 
